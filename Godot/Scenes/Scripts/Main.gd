e1b507d42060cd9d1c6e041737a63d3a59d86fff
LAK132 <LAK132@kicodora.com>
2018-04-25 19:05:16 +0930

    Merged two games

--- Godot/Scenes/Scripts/Main.gd
+++ Godot/Scenes/Scripts/Main.gd
@@ -7,40 +7,59 @@ onready var closeDoor = get_node("World/Door/ClosedDoor")
 
 var within_button = false
 
+onready var NodeEditor = get_node("NodeEditor")
+
+func door_open(open):
+	if open:
+		closeDoor.hide()
+		openDoor.show()
+	else:
+		closeDoor.show()
+		openDoor.hide()
+
 func _ready():
+	NodeEditor.addWorldTrigger("Door", funcref(self, "door_open"))
 	closeDoor.show()
 	get_node("PopupMenu/Resume").connect("pressed", self, "_resume")
 	get_node("PopupMenu/Quit").connect("pressed", self, "_quit")
 	buttonArea.connect("area_entered", self, "_playerEnterButton")
 	buttonArea.connect("area_exited", self, "_playerExitButton")
 	pass
-	
+
 func _resume():
 	menu.hide()
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pass
-	
+
 func _quit():
 	get_tree().quit()
 	pass
-	
+
 func _playerEnterButton(body):
 	within_button = true
 	pass
-	
+
 func _playerExitButton(body):
 	within_button = false
 	pass
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_menu"):
+		var editorcont = NodeEditor.get_node("EditorContainer")
+		if editorcont.is_visible_in_tree():
+			editorcont.hide()
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			editorcont.show()
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.is_action_just_pressed("ui_tab"):
 		if menu.is_visible_in_tree():
 			menu.hide()
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		else:
 			menu.show()
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
+
 	if within_button and Input.is_key_pressed(KEY_E):
 		if closeDoor.is_visible_in_tree():
 			closeDoor.hide()
--- Godot/Scenes/Scripts/Player.gd
+++ Godot/Scenes/Scripts/Player.gd
@@ -18,13 +18,13 @@ var velocity = Vector3()
 func _ready():
 	camera = $Rotation/Camera
 	camera_holder = $Rotation
-	
+
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
 	var direction = Vector3()
 	var cam_x = camera.get_global_transform()
-	
+
 	if Input.is_action_pressed("Forward"):
 		direction += -cam_x.basis.z.normalized()
 	if Input.is_action_pressed("Backward"):
@@ -33,33 +33,33 @@ func _physics_process(delta):
 		direction += -cam_x.basis.x.normalized()
 	if Input.is_action_pressed("Right"):
 		direction += cam_x.basis.x.normalized()
-		
+
 	if is_on_floor():
 		if Input.is_action_just_pressed("Jump"):
 			velocity.y = JUMP
-	
+
 	direction.y = 0
 	direction = direction.normalized()
-	
+
 	velocity.y += delta*GRAVITY
-	
+
 	var hvelocity = velocity
 	hvelocity.y = 0
-	
+
 	var target = direction
 	target *= SPEED
-	
+
 	var accel
 	if direction.dot(hvelocity) > 0:
 		accel = ACC
 	else:
 		accel = DEACC
-		
+
 	hvelocity = hvelocity.linear_interpolate(target, accel*delta)
 	velocity.x = hvelocity.x
 	velocity.z = hvelocity.z
 	velocity = move_and_slide(velocity, Vector3(0,1,0), 0.05, 4, deg2rad(SLOPE))
-	
+
 func _input(event):
 	if event is InputEventMouseMotion && Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		camera_holder.rotate_x(deg2rad(event.relative.y * SENSITIVITY * -1))
