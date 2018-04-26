3ca380df618ec59f1cd49c2c858441f75ec7ad60
Scobster007 <Scobster007@users.noreply.github.com>
2018-04-26 11:49:32 +0930

    update

--- Godot/Scenes/Scripts/Main.gd
+++ Godot/Scenes/Scripts/Main.gd
@@ -1,24 +1,43 @@
 extends Spatial
 
+onready var NodeEditor = get_node("NodeEditor")
+onready var NodeEditorVis = get_node("NodeEditor/EditorContainer")
 onready var menu = get_node("PopupMenu")
 onready var buttonArea = get_node("Button/Spatial/MeshInstance/Area")
 onready var openDoor = get_node("World/Door/OpenDoor")
 onready var closeDoor = get_node("World/Door/ClosedDoor")
+onready var openDoor2 = get_node("Door2/OpenDoor")
+onready var closeDoor2 = get_node("Door2/ClosedDoor")
 
 var within_button = false
+var connected = false
+var open = false
 
 func _ready():
+	NodeEditorVis.hide()
 	closeDoor.show()
+	closeDoor2.show()
 	get_node("PopupMenu/Resume").connect("pressed", self, "_resume")
 	get_node("PopupMenu/Quit").connect("pressed", self, "_quit")
 	buttonArea.connect("area_entered", self, "_playerEnterButton")
 	buttonArea.connect("area_exited", self, "_playerExitButton")
+	NodeEditor.addWorldTrigger("Door", funcref(self, "_connected"))
 	pass
 	
 func _resume():
 	menu.hide()
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pass
+	
+func _connected(door):
+	if door == true:
+		closeDoor.hide()
+		openDoor.show()
+		open = true
+	else:
+		open = false
+		openDoor.hide()
+		closeDoor.show()
 	
 func _quit():
 	get_tree().quit()
@@ -40,9 +59,22 @@ func _process(delta):
 		else:
 			menu.show()
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			
 	
-	if within_button and Input.is_key_pressed(KEY_E):
-		if closeDoor.is_visible_in_tree():
-			closeDoor.hide()
-			openDoor.show()
+	
+	if open == true and within_button and Input.is_action_just_pressed("interact"):
+		if closeDoor2.is_visible_in_tree():
+			closeDoor2.hide()
+			openDoor2.show()
+		elif openDoor2.is_visible_in_tree():
+			closeDoor2.show()
+			openDoor2.hide()
+			
+	if Input.is_action_just_pressed("ui_tab"):
+		if NodeEditorVis.is_visible_in_tree():
+			NodeEditorVis.hide()
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			NodeEditorVis.show()
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	pass
